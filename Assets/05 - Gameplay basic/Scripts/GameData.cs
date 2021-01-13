165eaa138c2ddfd05898e621d4b8bc1c44cf8e10
Eduard Ruesga <eruesga@xtec.cat>
2021-01-13 14:54:17 +0100

    Create new exercise: gameplay basics

---
+++ Assets/05 - Gameplay basic/Scripts/GameData.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Level
+{
+    [SerializeField]
+    public int Maxscore { get; set; }
+    public string Name { get; private set; }
+    public Scene numScene;
+
+}
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+
+public class GameData :ScriptableObject
+{
+    //In current Gameplay Data
+    [SerializeField] private int currentCoins;
+    [SerializeField] private int currentScore;
+
+    [SerializeField]
+    public int MaxScore { get; private set; }
+
+
+    public int CurrentCoins { get { return currentScore; } }
+    public int CurrentScore { get; }
+
+    public void UpdateMaxScore(int score)
+    {
+        score = MaxScore;
+    }
+
+    internal void ResetCurrentData()
+    {
+        InitCurrentData();
+    }
+
+    internal void InitCurrentData()
+    {
+        currentScore = 0;
+        currentCoins = 0;
+    }
+
+    public void UpdateCurrentScore(int score)
+    {
+        score += currentCoins;
+    }
+    public void UpdateCurrentCoints(int score)
+    {
+        score += currentScore;
+    }
+
+
+    //WIP
+    /* public int CurrentScore
+     {
+         get
+         {
+             return CurrentScore;
+         }
+         set
+         {
+             CurrentScore += value;
+         }
+     }
+     [SerializeField]
+     public float CurrentLives { get; set; }
+     public Level[] LevelsArray;*/
+
+}
---
+++ Assets/05 - Gameplay basic/Scripts/GameManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStates
+{
+    InitGame,
+    PlayingGame,
+    Pause,
+    Dead,
+    GameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _ins;
+    [SerializeField] private GameStates currentGameState;
+
+    public GameData gameData;
+
+
+    private void Awake()
+    {
+        if (_ins == null)
+        {
+            _ins = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Buscaremos el GObject que continene las referencias para iniciar las variables referenciadas del GameManager
+    void Start()
+    {
+        //Init the gameData to 0
+        gameData.InitCurrentData();
+    }
+
+    private void Update()
+    {
+        switch (currentGameState)
+        {
+            case GameStates.InitGame:
+                InitGame();     //UI con letras de nombre del nivel
+                break;
+            case GameStates.PlayingGame:
+                PlayingGame();  //Gameplay!
+                break;
+            case GameStates.Pause:
+                PauseGame();    //Haré salir la UI
+                break;
+            case GameStates.Dead:
+                HandleDead();   //Tiempo entre que se muere el personaje y cambiamos de pantalla o activamos UI.
+                break;
+            case GameStates.GameOver:
+                GameOver();     //Cambio de escena o activación de UI
+                break;
+        }
+    }
+
+    private void GameOver()
+    {
+        Debug.Log($"{this.gameObject.name} - GameOver!");
+    }
+
+    private void HandleDead()
+    {
+        Debug.Log($"{this.gameObject.name} - HandleDead!");
+    }
+
+    private void PauseGame()
+    {
+        Debug.Log($"{this.gameObject.name} - PauseGame!");
+    }
+
+    private void PlayingGame()
+    {
+        Debug.Log($"{this.gameObject.name} - PlayingGame!");
+    }
+
+    private void InitGame()
+    {
+        Debug.Log($"{this.gameObject.name} - InitGame!");
+    }
+
+    public bool isPlaying()
+    {
+        return (currentGameState == GameStates.PlayingGame);
+    }
+
+
+    
+
+
+}
